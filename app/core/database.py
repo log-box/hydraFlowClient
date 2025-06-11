527a3778f7bda14228f2867a2582962d15276715
log-box <a.y.logvin@gmail.com>
2025-06-11 07:43:27 +0300

    "Refactor: prepare for v2 release"

--- app/core/database.py
+++ app/core/database.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine, MetaData, select, desc, func, and_
 from sqlalchemy.orm import sessionmaker
+from tenacity import retry, wait_fixed, stop_after_delay, retry_if_exception_type
+from app.logger import logger
 
 
 class DBSettings:
@@ -15,18 +17,31 @@ class DBSettings:
 
 
 db_settings = DBSettings()
-
 engine = create_engine(db_settings.url)
-metadata = MetaData()
-metadata.reflect(bind=engine)
-
-flow = metadata.tables["hydra_oauth2_flow"]
 Session = sessionmaker(bind=engine)
+
+
+class FlowTableNotReady(Exception): pass
+
+@retry(
+    wait=wait_fixed(2),
+    stop=stop_after_delay(30),
+    retry=retry_if_exception_type(FlowTableNotReady)
+)
+def get_flow_table():
+    logger.info("Пробуем получить таблицу hydra_oauth2_flow...")
+    metadata = MetaData()
+    metadata.reflect(bind=engine, only=["hydra_oauth2_flow"])
+    if "hydra_oauth2_flow" not in metadata.tables:
+        logger.warning("Таблица ещё не готова, повторим позже.")
+        raise FlowTableNotReady("hydra_oauth2_flow отсутствует")
+    return metadata.tables["hydra_oauth2_flow"]
 
 
 def fetch_latest_flow_by_session_and_subject(login_session_id: str, subject: str) -> dict | None:
     session = Session()
     try:
+        flow = get_flow_table()
         query = (
             select(flow)
             .where(and_(
